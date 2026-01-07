0c8a840178f97bb35a348b92b4d06b9d0db326ba
Tejaswini Nagtode <71727139+tejaswininagtode@users.noreply.github.com>
2026-01-07 08:16:46 +0530

    Not_Boring_Movies.sql

--- SQL/Not_Boring_Movies.sql
+++ SQL/Not_Boring_Movies.sql
@@ -1,2 +1,15 @@
 select * from cinema where description not in ('boring') and id % 2 != 0 
 order by rating desc
+
+
+
+
+==========================
+
+
+SELECT *
+FROM cinema c
+WHERE c.id % 2 =1 
+    AND 
+    c.description != 'boring'
+ORDER BY c.rating DESC;
